29fe2b5161ea7fa2efd4e4552ebd74e5bc56756e
Zelin Chen <6692czl@gmail.com>
2023-02-02 16:40:53 -0800

    lab2-3

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-
+    val1 = int(input())
+    val2 = int(input())
+    val3 = int(input())
+    total = val1+val2+val3
+    avg = total/3
+    print('Values: {0} {1} {2}'.format(val1, val2, val3))
+    print('Total: {0:10d}'.format(total))
+    print('Average: {:.2f}'.format(avg))
     pass
 
 
